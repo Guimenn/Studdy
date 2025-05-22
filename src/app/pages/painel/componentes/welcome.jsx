fb36aefa7325a54d89c7a1a7532180023bf6b72c
Guilherme Vidichosqui Men <166948520+Guimenn@users.noreply.github.com>
2025-05-22 17:12:17 -0300

    commits

--- src/app/pages/painel/componentes/welcome.jsx
+++ src/app/pages/painel/componentes/welcome.jsx
@@ -19,8 +19,8 @@ export default function Welcome() {
                     />
                 </div>
                 <div>
-                    <h1 className="text-3xl font-bold bg-gradient-to-r from-primary to-primary/80 bg-clip-text text-transparent">Welcome back, Sarah!</h1>
-                    <p className="text-muted-foreground text-lg">Your mock exams are ready</p>
+                    <h1 className="text-3xl font-bold bg-gradient-to-r from-primary to-primary/80 bg-clip-text text-transparent">Olá, Sarah!</h1>
+                    <p className="text-muted-foreground text-lg">Veja oque há de novo</p>
                 </div>
             </div>
             <div className="relative hidden md:block">
